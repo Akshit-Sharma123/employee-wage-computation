4b73ff9a11129f8a4899c67403bc5660f6b7756c
Akshit-Sharma123 <as9008139@gmail.com>
2024-12-18 13:59:18 +0530

    [Akshit;add;check if employee is present/absent]

--- employeewage.cpp
+++ employeewage.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 int main(){
-    cout<<"Welcome to employeewage computation";
+    // int x=time(0);
+    // cout<<x;
+    cout<<"Welcome to employeewage computation"<<endl;
+    srand(time(0));
+    int n=rand()%2;
+    if(n==0){
+        cout<<"Attendance : 0"<<endl<<"Employee is absent"<<endl;
+    }
+    else{
+        cout<<"Attendance : 1"<<endl<<"Employee is present"<<endl;
+    }
+
+    
 }
