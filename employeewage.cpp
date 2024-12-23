ada6ade7c0987442fc42d7be243609ef9a499824
Akshit-Sharma123 <as9008139@gmail.com>
2024-12-24 03:21:11 +0530

    [Akshit;add;added uc4 switch case]

--- employeewage.cpp
+++ employeewage.cpp
@@ -3,26 +3,24 @@
 #include<ctime>
 using namespace std;
 int main(){
-    // int x=time(0);
-    // cout<<x;
     cout<<"Welcome to employeewage computation"<<endl;
     srand(time(0));
     int n=rand()%2;
-    // if(n==0){
-    //     //cout<<"Attendance : 0"<<endl<<"Employee is absent"<<endl;
-    //     cout<<"Daily wage of an employee is "<<20*8<<endl;
-    // }
-    // else{
-    //     //cout<<"Attendance : 1"<<endl<<"Employee is present"<<endl;
-    //     cout<<"Half day wage of an employee is "<<20*4<<endl;
-    // }
-    //uc4
     switch(n){
+        case 0:
+        cout<<"Attendance : 0"<<endl<<"Employee is absent"<<endl;
+        break;
+        case 1:
+         int d=rand()%2;
+        switch(d==0){
         case 0:
         cout<<"Daily wage of an employee is "<<20*8<<endl;
         break;
         case 1:
         cout<<"Part time wage of an employee is "<<20*4<<endl;
+       
+        }
     }
 
+    
 }
