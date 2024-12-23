717d9ee6a3d76a59803d90576e0bcc057f0a6f58
Akshit-Sharma123 <as9008139@gmail.com>
2024-12-23 13:39:53 +0530

    [Akshit ;add ;uc2 code]

--- employeewage.cpp
+++ employeewage.cpp
@@ -1,13 +1,20 @@
 #include<iostream>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
-int dailyWage(int w,int hr){
-    return w*hr;
-}
 int main(){
-    //calculate daily wage employee
-    int wage,hour;
-    cin>>wage>>hour;
-    cout<<"The daily wage of an employee is "<<dailyWage(wage,hour);
-    
+    // int x=time(0);
+    // cout<<x;
+    cout<<"Welcome to employeewage computation"<<endl;
+    srand(time(0));
+    int n=rand()%2;
+    if(n==0){
+        //cout<<"Attendance : 0"<<endl<<"Employee is absent"<<endl;
+        cout<<"Daily wage of an employee is "<<20*8<<endl;
+    }
+    else{
+        //cout<<"Attendance : 1"<<endl<<"Employee is present"<<endl;
+        cout<<"Half day wage of an employee is "<<20*4<<endl;
+    }
 
 }
