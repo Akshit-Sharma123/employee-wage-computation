7dbc14beb1d50136ac59057f7226e91205350191
Akshit-Sharma123 <as9008139@gmail.com>
2024-12-23 13:29:28 +0530

    [Akshit;add;uc2 cal daily wage]

--- employeewage.cpp
+++ employeewage.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 using namespace std;
+int dailyWage(int w,int hr){
+    return w*hr;
+}
 int main(){
-    cout<<"Welcome to employeewage computation";
+    //calculate daily wage employee
+    int wage,hour;
+    cin>>wage>>hour;
+    cout<<"The daily wage of an employee is "<<dailyWage(wage,hour);
+    
+
 }
