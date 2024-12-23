4d6fd2c16910f79bbabd57f8680da12c3ae53ea3
Akshit-Sharma123 <as9008139@gmail.com>
2024-12-23 15:31:07 +0530

    [Akshit;add;uc3 edited]

--- employeewage.cpp
+++ employeewage.cpp
@@ -2,11 +2,15 @@
 #include<cstdlib>
 #include<ctime>
 using namespace std;
+
 int main(){
     // int x=time(0);
     // cout<<x;
     cout<<"Welcome to employeewage computation"<<endl;
     srand(time(0));
+    //uc3
+    int f=rand()%2;
+    if(f==0){
     int n=rand()%2;
     if(n==0){
         //cout<<"Attendance : 0"<<endl<<"Employee is absent"<<endl;
@@ -16,5 +20,8 @@ int main(){
         //cout<<"Attendance : 1"<<endl<<"Employee is present"<<endl;
         cout<<"Half day wage of an employee is "<<20*4<<endl;
     }
-
+    }
+    else{
+    cout<<"Employee is absent "<<endl;
+    }
 }
