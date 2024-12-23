7456ec01f4ee5caf25f587952f79050bbf1f51db
Akshit-Sharma123 <as9008139@gmail.com>
2024-12-24 02:51:41 +0530

    [Akshit;add;added code of uc2 adding daily wage]

--- employeewage.cpp
+++ employeewage.cpp
@@ -3,18 +3,14 @@
 #include<ctime>
 using namespace std;
 int main(){
-    // int x=time(0);
-    // cout<<x;
     cout<<"Welcome to employeewage computation"<<endl;
     srand(time(0));
     int n=rand()%2;
     if(n==0){
-        //cout<<"Attendance : 0"<<endl<<"Employee is absent"<<endl;
-        cout<<"Daily wage of an employee is "<<20*8<<endl;
+        cout<<"Attendance : 0"<<endl<<"Employee is absent"<<endl;
     }
     else{
-        //cout<<"Attendance : 1"<<endl<<"Employee is present"<<endl;
-        cout<<"Half day wage of an employee is "<<20*4<<endl;
+       // cout<<"Attendance : 1"<<endl<<"Employee is present"<<endl;
+       cout<<"Daily wage of an employee is "<<20*8<<endl;
     }
-
 }
