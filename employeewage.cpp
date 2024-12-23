cbf0b907c64a08004833469db48173d51d0c701a
Akshit-Sharma123 <as9008139@gmail.com>
2024-12-23 15:25:32 +0530

    [Akshit;add;uc5 case edited]

--- employeewage.cpp
+++ employeewage.cpp
@@ -2,12 +2,15 @@
 #include<cstdlib>
 #include<ctime>
 using namespace std;
+
 int main(){
     // int x=time(0);
     // cout<<x;
     cout<<"Welcome to employeewage computation"<<endl;
     srand(time(0));
-    int n=rand()%2;
+    //int f=rand()%2;
+    //if(f==0){
+    //int n=rand()%2;
     // if(n==0){
     //     //cout<<"Attendance : 0"<<endl<<"Employee is absent"<<endl;
     //     cout<<"Daily wage of an employee is "<<20*8<<endl;
@@ -16,6 +19,10 @@ int main(){
     //     //cout<<"Attendance : 1"<<endl<<"Employee is present"<<endl;
     //     cout<<"Half day wage of an employee is "<<20*4<<endl;
     // }
+    //}
+    //else{
+    //cout<<"Employee is absent "<<endl;
+    //}
     //uc4
     // switch(n){
     //     case 0:
@@ -27,14 +34,41 @@ int main(){
 
     //uc5
     //calculating wages for a month
-    switch(n){
-        case 0:
-        cout<<"Full time monthly wage of an employee is "<<20*20*8<<endl;
-        break;
-        case 1:
-        cout<<"Part time monthly wage of an employee is "<<20*20*4<<endl;
+    int ans=0;
+    for(int i=0;i<20;i++){
+          int n=rand()%2;
+          if(n==0){
+            //cout<<"Employee is Present "<<endl;
+            int f=rand()%2;
+            if(f==0){
+                //fulltime
+                ans=ans+8*20;
+            }
+            else{
+                //parttime
+                ans=ans+4*20;
+            }
+          }
     }
+    cout<<"Calculating wages for a month "<<ans<<endl;
+    // switch(n){
+    //     case 0:
+    //     cout<<"Full time monthly wage of an employee is "<<20*20*8<<endl;
+    //     break;
+    //     case 1:
+    //     cout<<"Part time monthly wage of an employee is "<<20*20*4<<endl;
+    // }
+
+    
+    
+
+
+    
 }
+
+
+
+
 
 
 
